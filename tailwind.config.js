e4eb7aa219fbe964adab22222f88f1dc7c072100
jasn17 <Jas0n17L1n@gmail.com>
2024-10-14 18:34:44 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './build/*.js',
+    './build/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
